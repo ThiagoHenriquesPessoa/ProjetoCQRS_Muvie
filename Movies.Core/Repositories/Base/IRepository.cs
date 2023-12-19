d44e5574705c0757e225c5871620946adffba514
ThiagoHenriquesPessoa <thiagokxl5@gmail.com>
2023-12-19 09:43:14 -0300

    Repository of crud

--- Movies.Core/Repositories/Base/IRepository.cs
+++ Movies.Core/Repositories/Base/IRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Movies.Core.Repositories.Base
+﻿using Movies.Core.Entities.Base;
+
+namespace Movies.Core.Repositories.Base
 {
-    public interface IRepository
+    public interface IRepository<T> where T: Entity
     {
+        Task<IReadOnlyList<T>>tAllAsinc();
+        Task<T>GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
